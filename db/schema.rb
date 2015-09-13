003d60f735c1234467f53ad5278dd5a03fe6be53
Ben <bjlinville1@gmail.com>
2015-09-13 14:56:21 -0400

    added vote model & built relationship between topic and vote

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913184604) do
+ActiveRecord::Schema.define(version: 20150913185343) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150913184604) do
     t.integer  "votes"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
