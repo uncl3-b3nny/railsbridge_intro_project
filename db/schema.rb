bc8b7cd9215cb7d63078c557291f220a4c4e74cf
Ben <bjlinville1@gmail.com>
2015-12-16 21:08:01 -0500

    added feature model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914140953) do
+ActiveRecord::Schema.define(version: 20151217020706) do
+
+  create_table "features", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
